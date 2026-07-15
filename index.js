e21d19dc99604c8ab0e8d730c37ffc5de6e3d9af
Faizaditya-oss <faizaditya750@gmail.com>
2026-07-15 10:15:05 +0700

    GET: mengambil semua data dari tabel

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ const pool = new Pool({
 });
 
 
+// METHOD GET: Mengambil semua data dari tabel biodata
+app.get('/', (req, res) => {
+    console.log("TEST DATA: ");
+    pool.query('SELECT * FROM biodata')
+        .then(testData => {
+            console.log(testData.rows);
+            res.json(testData.rows);
+})
+  .catch (err => {
+    console.error("Error executing query", err.stack);
+    res.status(500).send("Database error");
+    });
+  
+});
