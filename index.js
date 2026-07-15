0814fbacaf69cc11ebd48c6c8d4d1a2a10976dcd
Faizaditya-oss <faizaditya750@gmail.com>
2026-07-15 10:13:21 +0700

    Menginisialisasi postgree

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import pg from 'pg';
+const { Pool } = pg;
+
 const app = express();
 const port = 3000;
+
+app.use(express.json()); // Middleware untuk parsing request body (JSON)
 
 const pool = new Pool({
   user: 'postgres',         
@@ -12,25 +16,3 @@ const pool = new Pool({
 });
 
 
-// METHOD GET: Mengambil semua data dari tabel biodata
-app.get('/', (req, res) => {
-    console.log("TEST DATA: ");
-    pool.query('SELECT * FROM biodata')
-        .then(testData => {
-            console.log(testData.rows);
-            res.json(testData.rows);
-})
-  .catch (err => {
-    console.error("Error executing query", err.stack);
-    res.status(500).send("Database error");
-    });
-  
-});
-
-// Jalankan server
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
-});
-
-//post
-//put
